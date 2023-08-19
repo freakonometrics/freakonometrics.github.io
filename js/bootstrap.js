ed013c33682f152380d718ef61c885f35b52ec44
Arthur Charpentier <arthur.charpentier@gmail.com>
2023-08-18 23:24:35 -0400

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2361,3 +2361,15 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+// Make Dropdown Submenus possible
+$('.dropdown-submenu a.dropdown-submenu-toggle').on("click", function(e){
+    $('.dropdown-submenu ul').removeAttr('style');
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+});
+// Clear Submenu Dropdowns on hidden event
+$('#bs-navbar-collapse-1').on('hidden.bs.dropdown', function () {
+  	$('.navbar-nav .dropdown-submenu ul.dropdown-menu').removeAttr('style');
+});
